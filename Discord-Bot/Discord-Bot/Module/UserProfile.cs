a6bfbe616aa3e9739fb0d0a83ce2ddab3bc371a5
Abdulla Y. Daniel <75367071+ardx3000@users.noreply.github.com>
2022-09-20 22:57:30 +0100

    fixed show profile
    replaced the old show profile method

---
+++ Discord-Bot/Discord-Bot/Module/UserProfile.cs
@@ -0,0 +1,59 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord_Bot.Module.Utilities;
+
+namespace Discord_Bot.Module;
+
+public class UserProfile : ModuleBase<SocketCommandContext>
+{
+    private SocketMessage socketMessage;
+    private TimeAndDate TnD = new TimeAndDate();
+    
+    
+    [Command("show profile")]
+    private async Task Profile()
+    {
+        
+        int points = 100;
+        var DiscordUser = socketMessage.Author;
+
+        var EmbedBuilderUserProfile = new EmbedBuilder().WithTitle(DiscordUser.Username)
+            .WithDescription("Points:" + points)
+            .WithImageUrl(DiscordUser.GetAvatarUrl())
+            .WithFooter(footer =>
+            {
+                footer
+                    .WithText("User: " + DiscordUser + " " + "Requested at: " + " " + TnD.Time);
+            });
+
+        Embed embedUP = EmbedBuilderUserProfile.Build();
+        
+        await ReplyAsync(embed: embedUP);
+
+    }
+
+    [Command("spt")]
+    private async Task ProfileTest(IGuildUser user = null)
+    {
+        if (user == null)
+        {
+           // user = self; set the user as the caster of the command
+        }
+
+        int points = 100;
+
+        var EmbedBuilderUserProfile = new EmbedBuilder()
+            .WithTitle($"{user}")
+            .WithImageUrl(user.GetDisplayAvatarUrl())
+            .WithDescription($"Points: {points}")
+            .WithFooter(footer =>
+            {
+                footer.WithText("User: " + user + " " + "Requested at: " + TnD.Time);
+            });
+        Embed embedUP = EmbedBuilderUserProfile.Build();
+        await ReplyAsync(embed: embedUP);
+    }
+
+    
+}
